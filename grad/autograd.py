b60ff80820c9fe6efca085c12ad9bced3dc70199
akv17 <temakep@list.ru>
2022-01-05 19:48:57 +0300

    add train routine

--- grad/autograd.py
+++ grad/autograd.py
@@ -1,4 +1,4 @@
-import pprint
+import logging
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Any
@@ -6,6 +6,13 @@ from uuid import uuid4
 from collections import defaultdict
 
 import numpy as np
+
+
+def get_logger(name=None, level='INFO'):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logging.basicConfig()
+    return logger
 
 
 def get_inner_ops(obj):
@@ -72,7 +79,7 @@ class Graph:
             if node.name not in tape:
                 tape[node.name] = grad
             else:
-                tape[node.name] += grad
+                tape[node.name].update(grad.data)
         return tape
 
 
@@ -295,12 +302,13 @@ class SGD:
 
 class Network:
 
-    def __init__(self, graph, loss, optimizer):
+    def __init__(self, graph, loss, optimizer, logger=None):
         self.graph = graph
         self.loss = loss
         self.optimizer = optimizer
         self.optimizer.assign_graph(self.graph)
         self._output = None
+        self._logger = logger or get_logger(type(self).__name__)
 
     def forward(self, x):
         output = self.graph.forward(x)
@@ -316,3 +324,28 @@ class Network:
 
     def step(self, grads):
         self.optimizer.step(grads)
+
+    def train(self, x, y, epochs=1, batch_size=64, verbose=None):
+        iter_num = 0
+        losses = []
+        for epoch in range(epochs):
+            for i in range(0, len(x.data), batch_size):
+                x_batch = x.data[i: i + batch_size]
+                x_batch = Tensor(name='x', data=x_batch, trainable=False)
+                y_batch = y.data[i: i + batch_size]
+                y_batch = Tensor(name='y', data=y_batch, trainable=False)
+                self.forward(x_batch)
+                backward_data = self.backward(y_batch)
+                loss = backward_data['loss'].data
+                losses.append(loss)
+                grads = backward_data['grads']
+                self.step(grads)
+                if verbose is not None and (iter_num + 1) % verbose == 0:
+                    msg = (
+                        f'epoch: {epoch+1}\t'
+                        f'iter: {iter_num+1}\t'
+                        f'loss: {np.mean(losses)}\t'
+                        f'batch_loss: {loss}'
+                    )
+                    self._logger.info(msg)
+                iter_num += 1
